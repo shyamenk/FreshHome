9542d9afd182e218180066583d4110c8b675645b
shyam <shyamenk@gmail.com>
2022-12-25 13:06:15 +0530

    project initialized with necessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    screen: {
+      sm: '480px',
+      md: '768px',
+      lg: '1020px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        primary: '#edf0f0',
+        secondary: '#2a3343',
+        third: '#d49836',
+        forth: '#9c523c',
+        text: '#949a9f',
+        fifth: '#dec39d',
+        sixth: '#c1c7cc',
+        seven: '#caa292',
+        eight: '#617265',
+      },
+    },
+  },
+  plugins: [],
+}
